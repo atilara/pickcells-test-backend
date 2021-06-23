3483f3452b7d80004893758eee27c20006a1d2f8
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-06-23 13:56:17 -0300

    fix: add missing elements

--- src/database/seeds/create_elements.ts
+++ src/database/seeds/create_elements.ts
@@ -16,6 +16,8 @@ export async function seed(knex: Knex) {
     { name: 'Estatística' },
     { name: 'Empreendedorismo e Inovação' },
     { name: 'Tópicos Avançados em Bancos de Dados' },
+    { name: 'Metodologia Científica' },
+    { name: 'Tópicos Avançados em Linguagem de Programação' },
   ]);
 
   await knex('course_classes').insert([
@@ -24,31 +26,42 @@ export async function seed(knex: Knex) {
     { course_id: 1, class_id: 2, workload: 4, mandatory: 1 },
     { course_id: 1, class_id: 3, workload: 2, mandatory: 0 },
     { course_id: 1, class_id: 5, workload: 6, mandatory: 1 },
+    { course_id: 1, class_id: 6, workload: 4, mandatory: 1 },
+    { course_id: 1, class_id: 7, workload: 6, mandatory: 0 },
     // SISTEMAS DE INFORMAÇÃO - Graduação
     { course_id: 2, class_id: 1, workload: 4, mandatory: 0 },
     { course_id: 2, class_id: 3, workload: 4, mandatory: 1 },
     { course_id: 2, class_id: 4, workload: 4, mandatory: 1 },
     { course_id: 2, class_id: 5, workload: 6, mandatory: 1 },
+    { course_id: 2, class_id: 6, workload: 4, mandatory: 1 },
+    { course_id: 2, class_id: 7, workload: 4, mandatory: 1 },
     // ENGENHARIA DE SOFTWARE - Mestrado
     { course_id: 3, class_id: 1, workload: 4, mandatory: 1 },
     { course_id: 3, class_id: 2, workload: 6, mandatory: 0 },
     { course_id: 3, class_id: 3, workload: 6, mandatory: 1 },
     { course_id: 3, class_id: 5, workload: 4, mandatory: 1 },
+    { course_id: 3, class_id: 6, workload: 6, mandatory: 1 },
+    { course_id: 3, class_id: 7, workload: 6, mandatory: 1 },
     // INTELIGÊNCIA COMPUTACIONAL - Mestrado
     { course_id: 4, class_id: 1, workload: 4, mandatory: 0 },
     { course_id: 4, class_id: 2, workload: 4, mandatory: 0 },
     { course_id: 4, class_id: 3, workload: 6, mandatory: 1 },
     { course_id: 4, class_id: 4, workload: 6, mandatory: 0 },
+    { course_id: 4, class_id: 6, workload: 6, mandatory: 1 },
+    { course_id: 4, class_id: 7, workload: 6, mandatory: 1 },
     // CIÊNCIA DA COMPUTAÇÃO - Doutorado
     { course_id: 5, class_id: 1, workload: 6, mandatory: 1 },
     { course_id: 5, class_id: 2, workload: 6, mandatory: 1 },
     { course_id: 5, class_id: 3, workload: 4, mandatory: 0 },
-    { course_id: 5, class_id: 5, workload: 4, mandatory: 4 },
+    { course_id: 5, class_id: 5, workload: 4, mandatory: 0 },
+    { course_id: 5, class_id: 6, workload: 6, mandatory: 1 },
+    { course_id: 5, class_id: 7, workload: 4, mandatory: 1 },
     // INTELIGÊNCIA ARTIFICAL - Doutorado
     { course_id: 6, class_id: 1, workload: 6, mandatory: 1 },
     { course_id: 6, class_id: 2, workload: 4, mandatory: 0 },
     { course_id: 6, class_id: 3, workload: 6, mandatory: 0 },
     { course_id: 6, class_id: 4, workload: 2, mandatory: 1 },
     { course_id: 6, class_id: 5, workload: 2, mandatory: 0 },
+    { course_id: 6, class_id: 6, workload: 6, mandatory: 1 },
   ]);
 }
